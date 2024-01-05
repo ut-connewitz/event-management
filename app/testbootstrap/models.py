71c3ea26244411c839de1e25f09194bef507cd91
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-01-05 16:26:33 +0100

    add testmodel and migration

--- app/testbootstrap/models.py
+++ app/testbootstrap/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    surname = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=30)
+    mail_adress = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Event(models.Model):
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    helper = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+
+
+class EventDay(models.Model):
+    pub_date = models.DateField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
