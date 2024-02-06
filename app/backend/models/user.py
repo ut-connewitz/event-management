6db230a3217290355913853933daef713c2bc8ec
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-02-06 18:08:05 +0100

    [#5] unique constraint for TeamMember class with exception handling

--- app/backend/models/user.py
+++ app/backend/models/user.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.utils import IntegrityError
 
 
 class User(models.Model):
@@ -67,8 +68,23 @@ class TeamMember(models.Model):
     team_name = models.ForeignKey(Team, on_delete=models.CASCADE)
 
     class Meta:
+        constraints = [
+        models.UniqueConstraint(fields=["username", "team_name"], name="prevent multiple memberships of one person in one team constraint")
+        ]
         verbose_name = "Teammitglied"
         verbose_name_plural = "Teammitglieder"
 
     def __str__(self):
         return str(self.team_name) + " " + str(self.username)
+
+    # overwrite save methods with caution!
+    # this is meant to prevent db crash if one person is added to the same team more than once from addtestdata.py
+    # this may occur, when addtestdata.py is run multiple times for development/testing reasons
+    # for now, only unique fields are problematic since primary key duplicates just wont be inserted (as expected) without crashing the db
+    # exception handling in the admin panel is not affected
+    def save(self, *args, **kwargs):
+        try:
+            super(TeamMember, self).save(*args, **kwargs)
+        except IntegrityError:
+            print("Person ist bereits in diesem Team")
+            pass
