62e4068b195f639b7880089d50c5827b1a41fbf6
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-06-19 15:05:11 +0200

    [#16] adjust deploy setup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput &
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+python3 manage.py addbasedata &
+
+python3 manage.py runserver 0.0.0.0:8000
 
 uwsgi --socket :8000 --master --enable-threads --module app.wsgi
