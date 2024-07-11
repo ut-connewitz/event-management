9e3dbfc01a0328af2896f005ce30ab43bd0b8aea
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-07-11 15:03:16 +0200

    [#13] show username on top of password change form

--- app/backend/views.py
+++ app/backend/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.forms import AdminPasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
 from django.views.generic.edit import FormView
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
@@ -38,7 +39,8 @@ class CustomAdminPasswordChangeView(PasswordContextMixin, FormView):
     success_url = reverse_lazy("password_change_done")
     # this template is an edited version of this django original template: https://github.com/django/django/blob/main/django/contrib/admin/templates/admin/auth/user/change_password.html
     template_name = "admin_change_pw.html"
-    title = _("TEST Password change")
+    new_pw_user = User()
+    title = _("Passwort ändern für: ")
 
     @method_decorator(sensitive_post_parameters())
     @method_decorator(csrf_protect)
@@ -46,20 +48,20 @@ class CustomAdminPasswordChangeView(PasswordContextMixin, FormView):
     def dispatch(self, request, *args, **kwargs):
         user=request.user
         new_pw_user_id = self.kwargs["pk"]
-        new_pw_user = User.objects.get(id=new_pw_user_id)
+        if new_pw_user_id:
+            new_pw_user = get_object_or_404(User, pk=new_pw_user_id)
+            self.new_pw_user = new_pw_user
+            self.title = _("Passwort ändern für: " + new_pw_user.get_username())
         if (
                 not user.is_staff
-                or (not user.is_superuser and (new_pw_user.is_superuser or new_pw_user.is_staff))
+                or (not user.is_superuser and (new_pw_user.is_superuser or new_pw_user.is_staff) and (user.id is not new_pw_user.id))
         ):
             raise PermissionDenied('Keine Berechtigung zum Ändern des Passworts')
         return super().dispatch(request, *args, **kwargs)
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        new_pw_user_id = self.kwargs["pk"]
-        new_pw_user = User.objects.get(id=new_pw_user_id)
-        #kwargs["user"] = self.request.user
-        kwargs["user"] = new_pw_user
+        kwargs["user"] = self.new_pw_user
         return kwargs
 
     def form_valid(self, form):
@@ -68,3 +70,15 @@ class CustomAdminPasswordChangeView(PasswordContextMixin, FormView):
         # except the current one.
         update_session_auth_hash(self.request, form.user)
         return super().form_valid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        new_pw_user_id = self.kwargs["pk"]
+        if new_pw_user_id:
+            new_pw_user = get_object_or_404(User, pk=new_pw_user_id)
+        context['username'] = new_pw_user.get_username()
+        context['test'] = "TEST"
+        context['userid'] = new_pw_user_id
+
+        return context
