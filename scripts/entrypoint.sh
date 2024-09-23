bdc9f4713445518978d42580131a90813ec9721d
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-09-23 15:22:21 +0200

    [#41] add createsuperuser, addbasedata and addtestdata to entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,4 +8,10 @@ python manage.py makemigrations
 
 python manage.py migrate
 
+python manage.py createsuperuser --noinput
+
+python manage.py addbasedata
+
+python manage.py addtestdata #remove for production deploy
+
 uwsgi --socket :44444 --master --enable-threads --module events.wsgi
